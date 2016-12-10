0f5ba4263c76f1fb2e3aa41faae1517bd7e4df1d
EkoMcCloud <d>
2016-12-10 12:20:36 +0100

    Init projet unity

---
+++ edkocorp-ld37-unity/Assets/Scripts/Characters/CharacterBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehavior : MonoBehaviour {
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+
+    public float speed = 1f;
+
+	// Use this for initialization
+	protected void Start ()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+
+    protected void Move(int xDir, int yDir)
+    {
+        Debug.Log("@@" + rb2D.ToString());
+        rb2D.transform.position += new Vector3(xDir * speed, yDir * speed);
+    }
+	
+}
---
+++ edkocorp-ld37-unity/Assets/Scripts/Characters/PlayerBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : CharacterBehavior {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+
+        int horizontal = (int)Input.GetAxisRaw("Horizontal");
+        int vertical = (int)Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            Move(horizontal, vertical);
+        }
+    }
+}
