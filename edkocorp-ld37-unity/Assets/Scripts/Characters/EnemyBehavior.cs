7921671559c99282aaafb7b7e49e19176f64bdef
EkoMcCloud <d>
2016-12-10 15:15:29 +0100

    Tests + Layouts

---
+++ edkocorp-ld37-unity/Assets/Scripts/Characters/EnemyBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : CharacterBehavior {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if(player != null)
+        {
+            LookAt(player.transform.position.x, player.transform.position.y);
+        }	
+	}
+}
