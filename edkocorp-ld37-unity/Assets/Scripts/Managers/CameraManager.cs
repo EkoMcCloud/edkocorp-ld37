48403e042035782d9557e8e196dccfd6826c95c8
EkoMcCloud <d>
2016-12-10 15:04:07 +0100

    Adding stuff (CameraController, Enemy Prefab)

---
+++ edkocorp-ld37-unity/Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    public static CameraManager instance;
+
+	// Use this for initialization
+	private void Awake () {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+	
+	// Update is called once per frame
+	private void LateUpdate () {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if(player != null)
+        {
+            Vector2 playerPosition = player.transform.position;
+            transform.position = new Vector3(playerPosition.x, playerPosition.y, transform.position.z);
+        }
+	}
+}
